5a6e74c8b45ae806b8aea42de526ee1fe520b0b1
JSNata <n.kulikouskaya@gmail.com>
2024-07-05 18:32:17 +0200

    chore: initial setup with Vite, TypeScript, ESLint, Prettier, and Husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint', 'jsx-a11y', 'react-refresh'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">App</p>
+    </>
+  );
+}
+
+export default App;
