377cdad73549319e555b16e79e3f4af67b2af4d6
JSNata <n.kulikouskaya@gmail.com>
2024-07-10 20:34:52 +0200

    feat: implement routing and 404 page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { useSearchParams } from 'react-router-dom';
 import ResultsList from '../components/ResultsList';
 import { People } from '../types/types';
 import Search from '../components/Search';
 import styles from './MainPage.module.css';
 import ErrorButton from '../components/ErrorButton';
 import Pagination from '../components/Pagination';
+import useSearchQuery from '../customHooks/useSearchQuery';
 
 const MainPage = () => {
   const [results, setResults] = useState<People[]>([]);
   const [loading, setLoading] = useState(false);
   const [hasError, setHasError] = useState(false);
   const [count, setCount] = useState(0);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useSearchQuery('searchQuery');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = parseInt(searchParams.get('page') || '1', 10);
 
   const fetchData = (page: number = 1, query: string = '') => {
     setLoading(true);
@@ -36,17 +40,18 @@ const MainPage = () => {
   };
 
   useEffect(() => {
-    const searchQuery = localStorage.getItem('searchQuery') || '';
+    setSearchParams({ page: currentPage.toString() });
     fetchData(currentPage, searchQuery);
-  }, [currentPage]);
+  }, [currentPage, searchQuery]);
 
   const searchHandler = (query: string) => {
-    setCurrentPage(1);
+    setSearchQuery(query);
+    setSearchParams({ page: '1' });
     fetchData(1, query);
   };
 
   const handlePageChange = (page: number) => {
-    setCurrentPage(page);
+    setSearchParams({ page: page.toString() });
   };
 
   if (hasError) {
@@ -70,5 +75,4 @@ const MainPage = () => {
     </div>
   );
 };
-
 export default MainPage;
