e03893bdbbfef577750061509342b01fa9bc3506
JSNata <n.kulikouskaya@gmail.com>
2024-08-16 13:37:26 +0200

    feat: implement new card higlight & styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { StoreFormData } from '../types/types';
+import styles from './Card.module.css';
 
-function Card({ ...data }: StoreFormData) {
+interface CardProps {
+  data: StoreFormData;
+  isNewFlag?: boolean;
+}
+
+function Card({ data, isNewFlag }: CardProps) {
   return (
-    <div>
-      <p>Name: {data.name}</p>
-      <p>Age: {data.age}</p>
-      <p>Email: {data.email}</p>
-      <p>Password: {data.password}</p>
-      <p>Gender: {data.gender}</p>
-      <p>T&C accepted: {data.acceptTerms.toString()}</p>
-      <p>Country: {data.country}</p>
-      <img
-        src={data.picture}
-        alt="Uploaded"
-        style={{ maxWidth: '200px', height: 'auto' }}
-      />
+    <div className={`${styles.card} ${isNewFlag ? styles.newCard : ''}`}>
+      <div className={styles.cardContent}>
+        <p>Name: {data.name}</p>
+        <p>Age: {data.age}</p>
+        <p>Email: {data.email}</p>
+        <p>Password: {data.password}</p>
+        <p>Gender: {data.gender}</p>
+        <p>T&C accepted: {data.acceptTerms.toString()}</p>
+        <p>Country: {data.country}</p>
+        <img alt="Pic" src={data.picture} className={styles.image} />
+      </div>
     </div>
   );
 }
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { RootState } from '../redux/store';
+import styles from './Main.module.css';
 import Card from '../components/Card';
 
 const Main = () => {
@@ -16,10 +17,15 @@ const Main = () => {
       <p>
         <Link to="/controlled">Controlled Form with React Hook Form</Link>
       </p>
-      <div>
-        <h3>Form Submissions:</h3>
+      <div className={styles.cards}>
+        <h3>Data</h3>
         {data.length > 0 ? (
-          data.map((data, index) => <Card key={index} {...data} />)
+          data
+            .slice()
+            .reverse()
+            .map((data, index) => (
+              <Card isNewFlag={index === 0} key={index} data={data} />
+            ))
         ) : (
           <p>No data</p>
         )}
